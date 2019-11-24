1e60a52cadde81c23b8200eb09d02276561fdd11
Andrei H <n3rd4i@gmail.com>
2019-11-24 11:07:28 +0100

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'ftp://ftp.fu-berlin.de/pc/games/idgames/levels/doom2/megawads/d2reload.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Join-Path $toolsDir wad)"
+  url           = $url
+  checksum      = 'A1C215CEC2013E972F4D52CB409A58203F576860F42398416D755F441B4019F4'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
